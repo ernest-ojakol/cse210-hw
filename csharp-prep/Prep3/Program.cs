9ab7cfae819f558b0064c57dea796d22f5d6b7fd
Ernest <ooquirrino@gmail.com>
2024-06-16 21:26:39 +0300

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Random randomGenerator = new Random();
+        //int number = randomGenerator.Next(1, 200);
+        Console.WriteLine("What is the magic number?");
+        string magic=Console.ReadLine();
+        int number=int.Parse(magic);
+
+        Console.WriteLine("What is your guess?");
+        string guess=Console.ReadLine();
+        int guessed_number=int.Parse(guess);
+        int guesses=0;
+
+        while(guessed_number!=number){
+            if(guessed_number>number){
+                Console.WriteLine("Higher");
+            }else{
+                Console.WriteLine("Lower");
+            }
+            Console.WriteLine("Please guess a number between 1 and 200");
+            guess=Console.ReadLine();
+            guessed_number=int.Parse(guess);
+            guesses++;
+        }
+        Console.WriteLine($"My number was {number}\nYour guess was {guessed_number}\nYou made {guesses} guesses");
     }
 }
