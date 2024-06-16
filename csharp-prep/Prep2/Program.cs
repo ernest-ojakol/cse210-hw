22d5d3801b31c22836900ff392972bdd2ed7cf12
Ernest <ooquirrino@gmail.com>
2024-06-16 21:08:51 +0300

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter Grade Percentage : ");
+        string percentage=Console.ReadLine();
+        int grade=int.Parse(percentage);
+        string letter="";
+
+        if(grade>=90){
+            letter="A";
+            Console.WriteLine("You passed!");
+        }else if(grade>=80 && grade<90){
+            letter="B";
+            Console.WriteLine("You passed!");
+        }else if(grade>=70 && grade<80){
+            letter="C";
+            Console.WriteLine("You passed!");
+        }else if(grade>=60 && grade<70){
+            letter="D";
+        }else if(grade<60){
+            letter="F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
